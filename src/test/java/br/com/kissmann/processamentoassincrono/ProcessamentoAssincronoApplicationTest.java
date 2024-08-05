5a922763b7f7a8fe36480878a43453aeae1545cf
Lucas Kissmann <lucaskissmann@gmail.com>
2024-08-05 09:02:47 -0300

    initial commit

---
+++ src/test/java/br/com/kissmann/processamentoassincrono/ProcessamentoAssincronoApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.kissmann.processamentoassincrono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoAssincronoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
